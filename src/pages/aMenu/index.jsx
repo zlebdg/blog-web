2086235f3ff36f76595db7007cda340e386af14a
xjplus <xxjplus@github.com>
2019-08-04 21:57:54 +0800

    引入md5和identicon, 准备绘制缺å省头像

--- src/pages/aMenu/index.jsx
+++ src/pages/aMenu/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { PageHeaderWrapper } from '@ant-design/pro-layout'
+import Identicon from 'identicon.js/identicon'
+import md5 from 'blueimp-md5/js/md5'
 
 /**
  * 菜单, 菜单的权限 都是umi根据路由实现自动化配置的
@@ -7,14 +9,31 @@ import { PageHeaderWrapper } from '@ant-design/pro-layout'
  * @returns {*}
  */
 
+console.log(md5('0'))
+
+const avatar0 = 'data:image/png;base64,' + new Identicon(md5(0), 64).toString()
+const avatar1 = 'data:image/png;base64,' + new Identicon(md5(1), 64).toString()
+const avatar2 = 'data:image/png;base64,' + new Identicon(md5(2), 64).toString()
+const avatar3 = 'data:image/png;base64,' + new Identicon(md5(3), 64).toString()
+const avatar4 = 'data:image/png;base64,' + new Identicon(md5(4), 64).toString()
+const avatar5 = 'data:image/png;base64,' + new Identicon(md5(5), 64).toString()
+
 export default () => (
   <PageHeaderWrapper>
-    <p
+    <div
       style={{
         textAlign: 'center',
       }}
     >
-      这是第一个菜单的测试页面
-    </p>
+      <h3>
+        这是第一个菜单的测试页面
+      </h3>
+      <img style={{ width: '64px' }} src={avatar0} alt="alt"/>
+      <img style={{ width: '64px' }} src={avatar1} alt="alt"/>
+      <img style={{ width: '64px' }} src={avatar2} alt="alt"/>
+      <img style={{ width: '64px' }} src={avatar3} alt="alt"/>
+      <img style={{ width: '64px' }} src={avatar4} alt="alt"/>
+      <img style={{ width: '64px' }} src={avatar5} alt="alt"/>
+    </div>
   </PageHeaderWrapper>
-);
+)
