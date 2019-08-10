46f1be8a595c596a2c39c1f41f1c12bf1d30b3bb
xjplus <xxjplus@github.com>
2019-08-10 15:42:06 +0800

    BraftEditor + ReactMarkdown

--- src/pages/markdown/Editor.jsx
+++ src/pages/markdown/Editor.jsx
@@ -5,40 +5,10 @@ import BraftEditor from 'braft-editor'
 import 'braft-editor/dist/index.css'
 
 class Editor extends React.Component {
-  state = {
-    // 创建一个空的editorState作为初始值
-    editorState: BraftEditor.createEditorState(null),
-  }
-
-  async componentDidMount() {
-    // 假设此处从服务端获取html格式的编辑器内容
-    // const htmlContent = await fetchEditorContent()
-    // 使用BraftEditor.createEditorState将html字符串转换为编辑器需要的editorStat
-    this.setState({
-      editorState: BraftEditor.createEditorState(''),
-    })
-  }
-
-  submitContent = async () => {
-    // 在编辑器获得焦点时按下ctrl+s会执行此方法
-    // 编辑器内容提交到服务端之前，可直接调用editorState.toHTML()来获取HTML格式的内容
-    const htmlContent = this.state.editorState.toHTML()
-    console.log(htmlContent)
-    // const result = await saveEditorContent(htmlContent)
-  }
-
-  handleEditorChange = (editorState) => {
-    this.setState({ editorState })
-  }
-
   render() {
-    const { editorState } = this.state
     return (
       <div className="my-component">
-        <BraftEditor
-          value={editorState}
-          onChange={this.handleEditorChange}
-          onSave={this.submitContent}/>
+        <BraftEditor/>
       </div>
     )
   }
--- src/pages/newBlog/NewBlog.jsx
+++ src/pages/newBlog/NewBlog.jsx
@@ -1,15 +1,67 @@
 import React from 'react'
 import { Col, Row } from 'antd'
+import * as showdown from 'showdown'
+import 'github-markdown-css'
+import ReactMarkdown from 'react-markdown/with-html'
+import BraftEditor from 'braft-editor'
+import 'braft-editor/dist/index.css'
+
+const content = `
+\`\`\`java
+function aa() {}
+\`\`\`
+
+<div>
+
+| Feature   | Support |
+| --------- | ------- |
+| tables    | ✔ |
+| 1 | ✔ |
+| 1 | ✔ |
+| 1 | ✔ |
+| 1 | ✔ |
+| 1 | ✔ |
+| 1 | ✔ |
+| 1 | ✔ |
+| 1 | ✔ |
+| 1 | x |
+| 1 | x |
+| 1 | x |
+| 1 | x |
+
+</div>
+
+hello
+<hr/>
+
+<h3>h3</h3>
+`
+
+const converter = new showdown.Converter()
+
+converter.setOption('omitExtraWLInCodeBlocks', false)
+converter.setOption('tables', true)
+converter.setOption('tasklists', true)
+converter.setOption('emoji', true)
+converter.setOption('underline', true)
+converter.setFlavor('github')
+
 
 class NewBlog extends React.Component {
   render() {
-    const { children } = this.props
     return (
       <div>
+        路由参数: { this.props.match.params.username }
         <Row justify="space-around" type="flex">
-          <Col>
-            路由参数: { this.props.match.params.username }
-            { children }
+          <Col md={ 12 } sm={ 24 }>
+            <BraftEditor/>
+          </Col>
+          <Col md={ 12 } sm={ 24 }>
+            <ReactMarkdown
+              className="markdown-body"
+              source={ converter.makeHtml(content) }
+              escapeHtml={ false }
+            />
           </Col>
         </Row>
       </div>
