1f70ba531a7c1801aea394dcfb577642506fea13
xuqplus <445172495@qq.com>
2019-08-28 11:49:18 +0800

    增加mock数据

--- mock/user.js
+++ mock/user.js
@@ -1,7 +1,7 @@
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
 export default {
   // 支持值为 Object 和 Array
-  'GET /api/currentUser': {
+  'GET /auth/currentUser': {
     name: 'Serati Ma',
     avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
     userid: '00000001',
